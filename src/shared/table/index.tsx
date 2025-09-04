353898244f2868a71fba2dd48fd299b1bfee63e9
Narmin131 <mnermin064@gmail.com>
2025-09-04 11:36:59 +0400

    `Refactor table styling and filter helpers`

--- src/shared/table/index.tsx
+++ src/shared/table/index.tsx
@@ -408,7 +408,7 @@ function Table<T extends Record<string, any>>({
                         display: 'flex',
                         alignItems: 'center',
                         justifyContent: 'space-between',
-                        color: isSelected ? '#0068F7' : headerTextStyle.color,
+                        color: isSelected ? 'var(--background-brand)' : toCssColor(headerTextStyle.color),
                         cursor: 'pointer',
                         height: headerConfig?.cell?.padding ? `${headerConfig?.cell?.padding}px` : '100%',
                         lineHeight: headerConfig?.cell?.padding ? `${headerConfig?.cell?.padding}px` : 'normal',
@@ -484,26 +484,6 @@ function Table<T extends Record<string, any>>({
             renderColumnActionsMenuItems: undefined,
         };
     });
-
-    function lightenColor(hex: string, percent: number): string {
-        const num = parseInt(hex.replace('#', ''), 16);
-        const amt = Math.round(2.55 * percent);
-        const R = (num >> 16) + amt;
-        const G = ((num >> 8) & 0x00ff) + amt;
-        const B = (num & 0x0000ff) + amt;
-
-        return (
-            '#' +
-            (
-                0x1000000 +
-                (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 +
-                (G < 255 ? (G < 1 ? 0 : G) : 255) * 0x100 +
-                (B < 255 ? (B < 1 ? 0 : B) : 255)
-            )
-                .toString(16)
-                .slice(1)
-        );
-    }
 
     const getNestedValue = (obj: any, path: string): any => path.split('.').reduce((acc, key) => acc?.[key], obj);
 
@@ -679,20 +659,20 @@ function Table<T extends Record<string, any>>({
             const mergedCell = rowCfg.cell || {};
             const mergedBorder = rowCfg.border || {};
 
-            const selectedBg = rowCfg?.selected?.backgroundColor ?? 'var(--background-selected)';
+            const selectedBg = 'var(--background-selected)';
 
             const getBackgroundColor = () => {
+                console.log(isRowSelected, 'isRowSelected');
                 if (isRowSelected) return selectedBg;
-
-                if (props.highlightedRowIds?.includes(row.id)) return '#E6F0FF';
 
                 const baseColor = toCssColor(mergedCell.backgroundColor) ?? '#ffffff';
                 const altColor = 'var(--background-secondary, #F3F3F3)';
 
                 if (hoveredRowId === row.id) {
-                    if (stripeStyle === 'plain') return altColor;
-                    const isOdd = row.index % 2 === 1;
-                    return isOdd ? altColor : baseColor;
+                    // if (stripeStyle === 'plain') return altColor;
+                    // const isOdd = row.index % 2 === 1;
+                    // return isOdd ? altColor : baseColor;
+                    return 'var(--background-selected)';
                 }
 
                 if (styleConfig.backgroundColor) return styleConfig.backgroundColor;
@@ -700,6 +680,8 @@ function Table<T extends Record<string, any>>({
 
                 return baseColor;
             };
+
+            getBackgroundColor();
 
             const style: React.CSSProperties = {
                 backgroundColor: getBackgroundColor(),
@@ -720,9 +702,9 @@ function Table<T extends Record<string, any>>({
                 textOverflow: 'ellipsis',
 
                 ...(isRowSelected && {
-                    borderLeftColor: ' #0068F7',
-                    ...(isFirst && { borderLeft: '1px solid #0068F7' }),
-                    ...(isLast && { borderRight: '1px solid #0068F7' }),
+                    borderLeftColor: 'var(--background-selected)',
+                    ...(isFirst && { borderLeft: '2px solid "var(--background-selected)"' }),
+                    ...(isLast && { borderRight: '2px solid "var(--background-selected)"' }),
                     zIndex: 1,
                 }),
             };
